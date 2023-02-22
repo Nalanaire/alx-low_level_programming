b78c1f30e59350fe0194041fbb1551828f13fab4
Nalanaire <senzonala@yahoo.com>
2023-02-22 11:40:16 -0800

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * add - adds two intergers
+ * @num1: first interger
+ * @num2: second interger
+ * Return: sum of @num1 and @num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
