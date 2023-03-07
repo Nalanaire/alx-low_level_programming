9d38cecdb4331e4c8c6513eee34fd342e90c4e97
Nalanaire <senzonala@yahoo.com>
2023-03-06 19:24:53 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-		int k;
+	int k;
 
-		while (*s)
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
 		{
-			for (k = 0; accept[k]; k++)
-			{
 			if (*s == accept[k])
 			return (s);
-			}
 		}
-		s++;
+	}
+	s++;
 
 	return ('\0');
 }
