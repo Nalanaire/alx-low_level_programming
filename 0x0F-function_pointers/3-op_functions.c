16bac3dc7281552901e358726b736d775c3d9c6b
Nalanaire <senzonala@yahoo.com>
2023-03-22 15:48:44 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculate the sum of the two intergers
+ * @a: firts interger
+ * @b: second interger
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculate the difference of the two intergers
+ * @a: firts interger
+ * @b: second interger
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculate the product of the two intergers
+ * @a: firts interger
+ * @b: second interger
+ * Return: difference of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculate the divisio of the two intergers
+ * @a: firts interger
+ * @b: second interger
+ * Return: difference of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mul - calculate the remainder of the two intergers
+ * @a: firts interger
+ * @b: second interger
+ * Return: difference of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
