cffec50ee19418718c96a31d4d6c4fd73bf907fc
Nalanaire <senzonala@yahoo.com>
2023-03-23 18:45:09 -0700

    Beauty is variable, ugliness is constant

--- alx-low_level_programming/0-sum_them_all.c
+++ alx-low_level_programming/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - returns the sum of all its parameters
  * @n: number of parameters
  * @...: a variable number of parameters to calculate the sum of.
- * @Return: if n == 0 - 0.
+ * Return: if n == 0 - 0.
  *	    Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
