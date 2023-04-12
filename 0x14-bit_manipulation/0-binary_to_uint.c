6f3fae50bdcd26d5d74a92138d14a777ecc20a81
Nalanaire <senzonala@yahoo.com>
2023-04-12 07:39:17 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -28,37 +28,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (result);
 }
-
-/**
- * _pow_recursion - A function to return a raised to power of b
- * @a: integer
- * @b: exponent
- * Return: the integer to be returned
- */
-
-int _pow_recursion(int a, int b)
-{
-	if (b < 0)
-		return (-1);
-	if (b == 0)
-		return (1);
-	return (a * _pow_recursion(a, b - 1));
-}
-
-/**
- * _strlen - A function to return the length of a string
- * @s: string
- * Return: the length of a string
- */
-
-int _strlen(const char *s)
-{
-        int i = 0;
-
-        while (*s != '\0')
-        {
-                i++;
-                s++;
-        }
-        return (i);
-}
